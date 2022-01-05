4abb89de626c6ba06376bcc22d4e4a17d7cfb3a1
khuong307 <85556946+khuong307@users.noreply.github.com>
2022-01-06 00:12:29 +0700

    update
    Approve New Bidder Request.
    Merge with Khang Dang 6/1/2022
    Merge with MinhLE update Category

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
+from pathlib import Path
+downloads_path = str(Path.home() / "Downloads")
+from rsa import * 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
 app = Flask(__name__)  
+authUsername = ""
 
 
 # khai báo hàm đọc danh sách user.
@@ -11,6 +20,38 @@ def readUserList():
         userList.append(tmp)
     file.close()
     return userList
+
+# khai báo hàm đọc danh sách khóa công khai.
+def readPublicKeyList():
+    publicKey = []
+    file = open("publicKey.txt", "r")
+    for line in file:
+        tmp = line.split('-')
+        publicKey.append(tmp)
+    file.close()
+    return publicKey
+
+# khai báo hàm đọc danh sách khóa bí mật.
+def readPrivateKeyList():
+    privateKey = []
+    file = open("privateKey.txt", "r")
+    for line in file:
+        tmp = line.split('-')
+        privateKey.append(tmp)
+    file.close()
+    return privateKey
+
+#lấy khóa bí mật bằng username
+def getPrivateByUsername(username, privateKey):
+    for keys in privateKey:
+        if keys[0] == username:
+            return keys
+
+#lấy khóa công khai bằng username
+def getPublicByUsername(username, publicKey):
+    for keys in publicKey:
+        if keys[0] == username:
+            return keys
 
 #khai báo thêm tài khoản user:
 def addNewUser(info):
@@ -46,6 +87,8 @@ def login():
         userList = readUserList()
         isUser = checkUser(username, password, userList)
         if (isUser == 1):
+            global authUsername 
+            authUsername = username
             return redirect(url_for("homeLogin")) #đăng nhập thành công -> vào trang chủ homeLogin.
         else:
             return render_template('login.html', err_message = True)
@@ -54,7 +97,31 @@ def login():
 #khai báo home (login thành công)
 @app.route('/homeLogin', methods=['GET', 'POST'])
 def homeLogin():
-    return render_template('home.html')
+    return render_template('home.html', username = authUsername)
+
+#khai báo viewImgs (xem toàn bộ ảnh)
+@app.route('/viewImgs', methods=['GET', 'POST'])
+def viewImgs():
+    if request.method == 'GET':
+        directory_path = 'static/uploads/' + authUsername
+        No_of_files = len(os.listdir(directory_path))
+        return render_template('viewImgs.html', username = authUsername, numberofFile = No_of_files)
+    if request.method == 'POST':
+        filename = request.args.get('filename')
+        privateKey = readPrivateKeyList()
+        keys = getPrivateByUsername(authUsername,privateKey)
+        print(keys)
+        downloads_path = str(Path.home() / "Downloads")
+
+        my_img = mpimg.imread('static/uploads/' + authUsername + '/'+filename)
+        DecryptionIMIG(527, 131, my_img, my_img)
+
+        
+        cv2.imwrite(downloads_path + '/' + filename, my_img)
+
+        directory_path = 'static/uploads/' + authUsername
+        No_of_files = len(os.listdir(directory_path))
+        return render_template('viewImgs.html',username = authUsername, numberofFile = No_of_files)
 
 #khai báo register.
 @app.route('/register', methods=['GET', 'POST'])  
@@ -75,7 +142,11 @@ def register():
         else:
             return render_template('register.html', err = True, message = "Username đã tồn tại!")
     return render_template('register.html', err = False)
-  
 
-if __name__ =="__main__":
-    app.run(debug = True)
+
+    
+
+
+
+if __name__ =="__main__":\
+    app.run('localhost', 5000)
